5c9d5968e5747c04bc3bd223e1ea6d0d04df46b4
Aman Sharma <amansharmavk9899@gmail.com>
2025-07-01 14:42:22 +0530

    09: agents setup

--- src/modules/home/ui/view/home-view.tsx
+++ src/modules/home/ui/view/home-view.tsx
@@ -1,14 +1,4 @@
 "use client";
-
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const {data}=useQuery(trpc.hello.queryOptions({text:"Aman"}));
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      {data?.greeting}
-    </div>
-  );
+  return <div>Home View</div>;
 };
