557dda814bebd34421353f84e8ced366989a6957
Aman Sharma <amansharmavk9899@gmail.com>
2025-07-14 04:42:02 +0530

    19: meetings filters

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -12,45 +12,43 @@ import {
 } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
+import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
-  update:protectedProcedure
-      .input(meetingsUpdateSchema)
-      .mutation(async ({ctx,input})=>{
-        const [updatedMeeting] = await db
-           .update(meetings)
-           .set(input)
-           .where(
-            and(
-              eq(meetings.id, input.id),
-              eq(meetings.userId, ctx.auth.user.id),
-            )
-           )
-           .returning();
-  
-           if(!updatedMeeting){
-             throw new TRPCError({
-               code: "NOT_FOUND",
-               message: "Meeting not found"
-             });
-           }
-  
-           return updatedMeeting;
-      }),
+  update: protectedProcedure
+    .input(meetingsUpdateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const [updatedMeeting] = await db
+        .update(meetings)
+        .set(input)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        )
+        .returning();
+
+      if (!updatedMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+
+      return updatedMeeting;
+    }),
   create: protectedProcedure
-      .input(meetingsInsertSchema)
-      .mutation(async ({ input, ctx }) => {
-        const [createdMeeting] = await db
-          .insert(meetings)
-          .values({
-            ...input,
-            userId: ctx.auth.user.id,
-          })
-          .returning();
-        
-        // TODO: Create stream call, upsert stream users
-        return createdMeeting;
-      }),
+    .input(meetingsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdMeeting] = await db
+        .insert(meetings)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      // TODO: Create stream call, upsert stream users
+      return createdMeeting;
+    }),
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input, ctx }) => {
@@ -82,23 +80,37 @@ export const meetingsRouter = createTRPCRouter({
           .max(MAX_PAGE_SIZE)
           .default(DEFAULT_PAGE_SIZE),
         search: z.string().nullish(),
+        agentId: z.string().nullish(),
+        status: z
+          .enum([
+            MeetingStatus.Upcoming,
+            MeetingStatus.Active,
+            MeetingStatus.Completed,
+            MeetingStatus.Processing,
+            MeetingStatus.Cancelled,
+          ])
+          .nullish(),
       })
     )
     .query(async ({ ctx, input }) => {
-      const { search, page, pageSize } = input;
+      const { search, page, pageSize, status, agentId } = input;
 
       const data = await db
         .select({
           ...getTableColumns(meetings),
           agent: agents,
-          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as(
+            "duration"
+          ),
         })
         .from(meetings)
-        .innerJoin(agents, eq(meetings.agentId,agents.id))
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined
           )
         )
         .orderBy(desc(meetings.createdAt), desc(meetings.id))
@@ -108,11 +120,13 @@ export const meetingsRouter = createTRPCRouter({
       const [total] = await db
         .select({ count: count() })
         .from(meetings)
-        .innerJoin(agents, eq(meetings.agentId,agents.id))
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined
           )
         );
       const totalPages = Math.ceil(total.count / pageSize);
---
+++ src/modules/meetings/ui/components/status-filter.tsx
@@ -0,0 +1,80 @@
+import {
+  CircleXIcon,
+  CircleCheckIcon,
+  ClockArrowUpIcon,
+  VideoIcon,
+  LoaderIcon,
+} from "lucide-react";
+
+import { CommandSelect } from "@/components/command-select";
+
+import { MeetingStatus } from "../../types";
+
+import { useMeetingsFilter } from "../../hooks/use-meetings-filters";
+
+const options = [
+  {
+    id: MeetingStatus.Upcoming.toString(),
+    value: MeetingStatus.Upcoming.toString(),
+    children: (
+      <div className="flex items-center gap-x-2 capitalize">
+        <ClockArrowUpIcon />
+        {MeetingStatus.Upcoming}
+      </div>
+    ),
+  },
+  {
+    id: MeetingStatus.Completed.toString(),
+    value: MeetingStatus.Completed.toString(),
+    children: (
+      <div className="flex items-center gap-x-2 capitalize">
+        <CircleCheckIcon />
+        {MeetingStatus.Completed}
+      </div>
+    ),
+  },
+  {
+    id: MeetingStatus.Active.toString(),
+    value: MeetingStatus.Active.toString(),
+    children: (
+      <div className="flex items-center gap-x-2 capitalize">
+        <VideoIcon />
+        {MeetingStatus.Active}
+      </div>
+    ),
+  },
+  {
+    id: MeetingStatus.Processing.toString(),
+    value: MeetingStatus.Processing.toString(),
+    children: (
+      <div className="flex items-center gap-x-2 capitalize">
+        <LoaderIcon />
+        {MeetingStatus.Processing}
+      </div>
+    ),
+  },
+  {
+    id: MeetingStatus.Cancelled.toString(),
+    value: MeetingStatus.Cancelled.toString(),
+    children: (
+      <div className="flex items-center gap-x-2 capitalize">
+        <CircleXIcon />
+        {MeetingStatus.Cancelled}
+      </div>
+    ),
+  },
+];
+
+export const StatusFilter = () => {
+  const [filters, setFilters] = useMeetingsFilter();
+
+  return (
+    <CommandSelect
+      placeholder="Status"
+      className="h-9"
+      options={options}
+      onSelect={(value) => setFilters({ status: value as MeetingStatus })}
+      value={filters.status ?? ""}
+    />
+  );
+};
